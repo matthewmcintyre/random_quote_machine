7e3861ff747468be4b7c8bfb9aec220f9cea3c0b
Matthew McIntyre <mattymac2@gmail.com>
2019-03-08 15:36:15 +0000

    Updates

---
+++ precache-manifest.c743150e0612abcfc078bd09c77a0305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d06ad02754571fe69351",
+    "url": "/random_quote_machine/static/css/main.e65264b2.chunk.css"
+  },
+  {
+    "revision": "d06ad02754571fe69351",
+    "url": "/random_quote_machine/static/js/main.30dbf6bc.chunk.js"
+  },
+  {
+    "revision": "64427c485b72af5154ef",
+    "url": "/random_quote_machine/static/js/runtime~main.218f8e92.js"
+  },
+  {
+    "revision": "e8e0b032567894f2ca30",
+    "url": "/random_quote_machine/static/js/2.662d46c0.chunk.js"
+  },
+  {
+    "revision": "b48340dbefc5d47b69074de0eaed7db7",
+    "url": "/random_quote_machine/index.html"
+  }
+];
